22a0e90b217262a43359137fdacaf47646752358
张坤 <929794551@qq.com>
2022-10-30 09:56:38 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  {{ countStore.count }}
+  <hr>
+  {{ countStore.doubleCount }}
+  <hr>
+  <button @click="countStore.increment">increment</button>
+</template>
+<script setup>
+  import { useCounterStore } from "./stores/counter"
+  const countStore = useCounterStore()
+</script>
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia().use(piniaPluginPersistedstate))
+
+app.mount('#app')
